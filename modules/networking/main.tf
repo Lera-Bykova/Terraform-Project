d03bae02ad28714a4b964480d3c2d3da1b5cc010
Lera_Bykova <valeria.bykova@outlook.com>
2024-03-04 15:29:11 +0000

    Adding NAT gateway route with EIP, as well as security groups

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -36,10 +36,20 @@ resource "aws_internet_gateway" "gw" {
   }
 }
 
+# Elastic IP address -> public IPv4 address -> With an Elastic IP address, you can mask the failure of an instance by rapidly remapping the address to another instance in your VPC.
+resource "aws_eip" "nat_gateway" {
+  domain = "vpc"
+    #  ^---- Indicates if this EIP is for use in VPC
+}
+
 # Nat gateways
 resource "aws_nat_gateway" "project_nat" {
-  connectivity_type = "private"
-  subnet_id         = aws_subnet.private[0].id
+  connectivity_type = "public"
+  allocation_id = aws_eip.nat_gateway.id
+  subnet_id         = aws_subnet.public[0].id
+  tags = {
+    Name = "gw NAT"
+  }
 }
 
 # Route Tables
@@ -54,7 +64,6 @@ resource "aws_route_table" "route_table_public" {
   }
 }
 resource "aws_route_table" "route_table_private" {
-    count = 3
     vpc_id = aws_vpc.project_vpc.id
     route {
         cidr_block = "0.0.0.0/0"
@@ -62,7 +71,7 @@ resource "aws_route_table" "route_table_private" {
     }
 
       tags = {
-    Name = "route_table_private${count.index}"
+    Name = "route_table_private"
   }
 
 }
@@ -77,5 +86,5 @@ resource "aws_route_table_association" "public" {
 resource "aws_route_table_association" "private" {
     count = 3
   subnet_id      = aws_subnet.private[count.index].id
-  route_table_id = aws_route_table.route_table_private[count.index].id
+  route_table_id = aws_route_table.route_table_private.id
 }
